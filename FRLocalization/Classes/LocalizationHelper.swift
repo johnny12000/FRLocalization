96b6d4011a657ad14b9219c67532137d0567179b
johnny12000 <ristic.nikola@icloud.com>
2017-08-05 16:21:00 +0200

    Class and method accessor modifier changed

--- FRLocalization/Classes/LocalizationHelper.swift
+++ FRLocalization/Classes/LocalizationHelper.swift
@@ -16,18 +16,19 @@ let DefaultLanguage = "en"
 let LocalizationChanged = "LocalizationChanged"
 
 /// Manager for localization features of the app.
-class LocalizationHelper: NSObject {
-    
+public class LocalizationHelper: NSObject {
+
     let userDefaults: UserDefaults
-    
+
     init(userDefaults: UserDefaults = UserDefaults.standard) {
         self.userDefaults = userDefaults
     }
-    
-    static var defaultHelper: LocalizationHelper = LocalizationHelper()
+
+    public static var defaultHelper: LocalizationHelper = LocalizationHelper()
+
     /// Default language
     /// - returns: The app's default language. String.
-    func defaultLanguage() -> String {
+    public func defaultLanguage() -> String {
         var defaultLanguage: String = String()
         guard let preferredLanguage = Bundle.main.preferredLocalizations.first else {
             return DefaultLanguage
@@ -40,52 +41,52 @@ class LocalizationHelper: NSObject {
         }
         return defaultLanguage
     }
-    
+
     /// Resets the current language to the default
-    func resetCurrentLanguageToDefault() {
+    public func resetCurrentLanguageToDefault() {
         currentLanguage = defaultLanguage()
     }
-    
+
     /// Get the current language's display name for a language.
     /// - parameter language: Desired language.
     /// - returns: The localized string.
-    func displayName(language: String) -> String {
+    public func displayName(language: String) -> String {
         let locale: NSLocale = NSLocale(localeIdentifier: currentLanguage)
         if let displayName = locale.displayName(forKey: NSLocale.Key.identifier, value: language) {
             return displayName
         }
         return String()
     }
-    
-    var currentLanguageWithoutLocale: String {
+
+    public var currentLanguageWithoutLocale: String {
         let index = currentLanguage.index(currentLanguage.startIndex, offsetBy: 2)
         return currentLanguage.substring(to: index)
     }
-    
-    var appleLanguages: [String] {
+
+    public var appleLanguages: [String] {
         return userDefaults.object(forKey: AppleLanguageKey) as? [String] ?? []
     }
-    
-    var availableLanguages: [String] {
+
+    public var availableLanguages: [String] {
         var availableLanguages = Bundle.main.localizations
         if let indexOfBase = availableLanguages.index(of: "Base") {
             availableLanguages.remove(at: indexOfBase)
         }
         return availableLanguages
     }
-    
-    var currentLanguage: String {
+
+    public var currentLanguage: String {
         get {
             for language in appleLanguages {
                 if availableLanguages.contains(language) {
                     return language
                 }
             }
-            
+
             return defaultLanguage()
         }
         set {
-            if newValue != currentLanguage && availableLanguages.contains(newValue)  {
+            if newValue != currentLanguage && availableLanguages.contains(newValue) {
                 var appleLanguagesArray = appleLanguages
                 if let index = appleLanguagesArray.index(of: newValue) {
                     appleLanguagesArray.remove(at: index)
